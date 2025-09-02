a94c5ed277d4d7c4658b35379b69fac6ba9f7584
Andreza Santos <andrezaumbelino@users.noreply.github.com>
2025-09-02 20:42:11 +0100

    finalizing nps score

--- 04-Decision-Science/01-Project-Setup/data-context-and-setup/olist/order.py
+++ 04-Decision-Science/01-Project-Setup/data-context-and-setup/olist/order.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from olist.utils import haversine_distance
 from olist.data import Olist
-
 
 class Order:
     '''
@@ -20,105 +21,84 @@ class Order:
         and filters out non-delivered orders unless specified
         """
         # Hint: Within this instance method, you have access to the instance of the class Order in the variable self, as well as all its attributes
+        
         orders = self.data['orders'].copy()
-        if is_delivered:
-            orders = orders.query("order_status=='delivered'").copy()
+        orders_delivered = orders[orders['order_status'] == "delivered"].copy()
 
-        orders.loc[:, 'order_delivered_customer_date'] = pd.to_datetime(orders['order_delivered_customer_date'])
-        orders.loc[:, 'order_estimated_delivery_date'] = pd.to_datetime(orders['order_estimated_delivery_date'])
-        orders.loc[:, 'order_purchase_timestamp'] = pd.to_datetime(orders['order_purchase_timestamp'])
-
-        orders.loc[:, 'delay_vs_expected'] = \
-            (orders['order_delivered_customer_date'] -
-             orders['order_estimated_delivery_date']) / np.timedelta64(24, 'h')
-
-        def handle_delay(x):
-            # We only want to keep delay where wait_time is longer than expected (not the other way around)
-            # This is what drives customer dissatisfaction!
-            if x > 0:
-                return x
-            else:
-                return 0
-
-        orders.loc[:, 'delay_vs_expected'] = \
-            orders['delay_vs_expected'].apply(handle_delay)
-
-        # compute wait time
-        orders.loc[:, 'wait_time'] = \
-            (orders['order_delivered_customer_date'] -
-             orders['order_purchase_timestamp']) / np.timedelta64(24, 'h')
-
+        time_cols = ['order_purchase_timestamp','order_approved_at', 'order_delivered_carrier_date',              'order_delivered_customer_date', 'order_estimated_delivery_date']
+        
+        orders_delivered[time_cols] = orders_delivered[time_cols].apply(pd.to_datetime)
+        
+         # compute wait time
+        orders_delivered['wait_time'] = orders_delivered['order_delivered_customer_date'] -         orders_delivered['order_purchase_timestamp']
+        
+        orders_delivered['wait_time'] = orders_delivered['wait_time'].dt.days
+        
         # compute expected wait time
-        orders.loc[:, 'expected_wait_time'] = \
-            (orders['order_estimated_delivery_date'] -
-             orders['order_purchase_timestamp']) / np.timedelta64(24, 'h')
+        orders_delivered['expected_wait_time'] = orders_delivered['order_estimated_delivery_date'] -         orders_delivered['order_purchase_timestamp']
+        
+        orders_delivered['expected_wait_time'] = orders_delivered['expected_wait_time'].dt.days
+        
+        # compute delay_vs_expected
+        orders_delivered['delay_vs_expected'] = orders_delivered['order_delivered_customer_date']   -     orders_delivered['order_estimated_delivery_date']
+        
+        orders_delivered['delay_vs_expected'] = orders_delivered['delay_vs_expected'].dt.days
+        
+        orders_delivered['delay_vs_expected'] = np.where(orders_delivered['delay_vs_expected'] < 1,0,                         orders_delivered['delay_vs_expected'])
+        
+        orders_delivered = orders_delivered[[
+            'order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected','order_status']]
 
-        return orders[[
-            'order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected',
-            'order_status'
-        ]]
-
-
+        return orders_delivered
 
     def get_review_score(self):
-        """
-        Returns a DataFrame with:
-        order_id, dim_is_five_star, dim_is_one_star, review_score
-        """
-        reviews = self.data['order_reviews'].copy()
+         
+            reviews = self.data['order_reviews'].copy()
+            
+            reviews['dim_is_five_star'] =  np.where(reviews['review_score'] == 5,1,0)
+            reviews['dim_is_one_star'] =  np.where(reviews['review_score'] == 1,1,0)
+            reviews = reviews[['order_id', 'dim_is_five_star', 'dim_is_one_star', 'review_score']]
+            
+            return reviews
 
-        def dim_five_star(d):
-            if d == 5:
-                return 1
-            else:
-                return 0
-
-        def dim_one_star(d):
-            if d == 1:
-                return 1
-            else:
-                return 0
-
-        reviews.loc[:, 'dim_is_five_star'] =\
-            reviews['review_score'].apply(dim_five_star)
-
-        reviews.loc[:, 'dim_is_one_star'] =\
-            reviews['review_score'].apply(dim_one_star)
-
-        return reviews[[
-            'order_id', 'dim_is_five_star', 'dim_is_one_star', 'review_score'
-        ]]
 
     def get_number_items(self):
         """
         Returns a DataFrame with:
         order_id, number_of_items
         """
-        data = self.data
-        items = data['order_items'].groupby('order_id', as_index=False).agg({'order_item_id': 'count'})
-        items.columns = ['order_id', 'number_of_items']
-        return items
-
+        orders_item = self.data['order_items'].copy()
+        orders_item2 = orders_item.groupby(['order_id']).count().reset_index()
+        orders_item_final = orders_item2[['order_id','order_item_id']]
+        orders_item_final.rename(columns={"order_item_id": "number_of_items"},  inplace=True)
+        
+        return orders_item_final
+        
 
     def get_number_sellers(self):
         """
         Returns a DataFrame with:
         order_id, number_of_sellers
         """
-        data = self.data
-        sellers = data['order_items'].groupby('order_id')['seller_id'].nunique().reset_index()
-        sellers.columns = ['order_id', 'number_of_sellers']
-        return sellers
+        order_items2 = self.data['order_items'].copy()
+        order_items3 = order_items2[['order_id', 'seller_id']]
+        orders_num_sellers = order_items3.groupby(['order_id']).count().reset_index()
+        orders_num_sellers.rename(columns={"seller_id": "number_of_sellers"}, inplace=True)
+        
+        return orders_num_sellers
+     
 
     def get_price_and_freight(self):
         """
         Returns a DataFrame with:
         order_id, price, freight_value
         """
-        data = self.data
-        price_freight = data['order_items'].groupby('order_id', as_index=False).agg({'price': 'sum', 'freight_value': 'sum'})
-        return price_freight
-
+        order_items_freight = self.data['order_items'][['order_id','price','freight_value']].copy()
+        order_items_freight =  order_items_freight.groupby(by=["order_id"]).sum().reset_index()
+          
+        return order_items_freight
+        
+   
 
     # Optional
     def get_distance_seller_customer(self):
@@ -139,18 +119,8 @@ class Order:
         'distance_seller_customer']
         """
         # Hint: make sure to re-use your instance methods defined above
-        training_set = self.get_wait_time(is_delivered).merge(
-                self.get_review_score(), on='order_id'
-            ).merge(
-                self.get_number_items(), on='order_id'
-            ).merge(
-                self.get_number_sellers(), on='order_id'
-            ).merge(
-                self.get_price_and_freight(), on='order_id'
-            )
-        # Skip heavy computation of distance_seller_customer unless specified
-        if with_distance_seller_customer:
-            training_set = training_set.merge(
-                self.get_distance_seller_customer(), on='order_id')
-
+        
+        
+        training_set = self.get_wait_time(is_delivered).merge(self.get_review_score(), how='inner', on='order_id').merge(      self.get_number_items(), how='inner', on='order_id').merge(self.get_number_sellers(), how='inner', on='order_id').merge(    self.get_price_and_freight(), how='inner', on='order_id')
+        
         return training_set.dropna()
